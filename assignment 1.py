f47c76d5a549e0d18a4d90eef1e7a6cb2701afa7
Dhanaseela <138307993+Dhanaseela@users.noreply.github.com>
2023-07-01 21:06:09 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,9 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age = int(input("Enter your age: "))
+... 
+... if age < 18:
+...     print("You are a minor")
+... elif age >= 18 and age < 65:
+...     print("You are an adult")
+... else:
